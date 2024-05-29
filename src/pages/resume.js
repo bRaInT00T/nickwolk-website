bbb97d429453b4a33b91fb79805504fa9a3f97fd
Nick Wolk <nwwolk@gmail.com>
2024-05-29 17:06:08 -0400

    resume download format split button flip effect on toggle button update resume add docx version of resume move button css into resume.css

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import resumeFile from '../assets/Nicholas_Wolk_Resume.pdf'; // Make sure to upload the resume PDF to your project
 import BubbleText from "../components/BubbleText.js";
+import { FaFileWord, FaFilePdf } from 'react-icons/fa';
+import resumePdf from '../assets/Nicholas_Wolk_Resume.pdf';
+import resumeDocx from '../assets/Nicholas_Wolk_Resume.docx';
+import '../css/resume.css';
 
 const ResumeDownload = () => {
   return (
     <div className="text-container resume-download-container">
       <BubbleText initialText="Download My Resume" headingLevel="h1" />
       <p>For a detailed look at my professional background and achievements, please download my resume.</p>
-      <a href={resumeFile} download="Nicholas_Wolk_Resume.pdf" className="download-button">
+        <a href={resumePdf} download="Nicholas_Wolk_Resume.pdf" className="resume-download-button">
         Download Resume
       </a>
+      <div className="resume-split-button">
+        <div className="resume-dropdown">
+          <button className="resume-dropdown-toggle">
+            <span>▼</span>
+          </button>
+          <div className="resume-dropdown-menu">
+            <a href={resumePdf} download="Nicholas_Wolk_Resume.pdf"><FaFilePdf /> PDF</a>
+            <a href={resumeDocx} download="Nicholas_Wolk_Resume.docx"><FaFileWord/>DOCX</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
